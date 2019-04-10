9136b8ad874d60c136bfcc379c8b51c5fa8bf095
kitchWWW <brian.e2014@gmail.com>
2019-04-10 12:26:42 -0500

    changed to spaces

--- genSamps_3.py
+++ genSamps_3.py
@@ -9,52 +9,52 @@ filesInSamps2 = sorted(os.listdir('samps_2'))
 
 def gen(prefix,totalToTake):
 
-	#### LOAD DATA
-	xdic = {}
-	count = 0
+    #### LOAD DATA
+    xdic = {}
+    count = 0
 
-	for f in filesInSamps2:
-	    if f == '.DS_Store':
-	        continue
-	    myImage = misc.imread('samps_2/'+f)
-	    if f[0] == prefix:
-	    	count+=1
-	    	xSize = len(myImage.flatten())
-	        xdic[f] = (myImage.flatten() / divBy) - middleNorm
-	        if count >= totalToTake:
-	        	break
+    for f in filesInSamps2:
+        if f == '.DS_Store':
+            continue
+        myImage = misc.imread('samps_2/'+f)
+        if f[0] == prefix:
+            count+=1
+            xSize = len(myImage.flatten())
+            xdic[f] = (myImage.flatten() / divBy) - middleNorm
+            if count >= totalToTake:
+                break
 
-	x = []
-	for k in sorted(xdic.keys()):
-		x.append(xdic[k])
+    x = []
+    for k in sorted(xdic.keys()):
+        x.append(xdic[k])
 
-	xnp = np.array(x)
-	xlen = xnp.shape[0]
+    xnp = np.array(x)
+    xlen = xnp.shape[0]
 
-	xret = xnp.reshape(xlen,xSize)
-	return xret
+    xret = xnp.reshape(xlen,xSize)
+    return xret
 
 
 
 def revy(inArr,ittrNumb,fileNo,suf,outdir):
-	arr = np.copy(inArr)
-	arr += middleNorm
-	arr *= divBy
-	newImage = np.reshape(arr, (33,33,3))
-	# misc.imsave('out/'+str(ittrNumb)+suf+".jpg",newImage)
-	# print("hello?")
-	if "samp" in suf :
-		# print(ittrNumb,fileNo,suf,filesInSamps2[fileNo][1:])
-		compositeImage = misc.imread('samps_1/'+filesInSamps2[fileNo][1:])
-		misc.imsave(outdir+''+str(ittrNumb)+suf+"_orig.jpg",compositeImage)
-		compositeImage[33:66,33:66] = newImage
-		misc.imsave(outdir+''+str(ittrNumb)+suf+"_comp.jpg",compositeImage)
+    arr = np.copy(inArr)
+    arr += middleNorm
+    arr *= divBy
+    newImage = np.reshape(arr, (33,33,3))
+    # misc.imsave('out/'+str(ittrNumb)+suf+".jpg",newImage)
+    # print("hello?")
+    if "samp" in suf :
+        # print(ittrNumb,fileNo,suf,filesInSamps2[fileNo][1:])
+        compositeImage = misc.imread('samps_1/'+filesInSamps2[fileNo][1:])
+        misc.imsave(outdir+''+str(ittrNumb)+suf+"_orig.jpg",compositeImage)
+        compositeImage[33:66,33:66] = newImage
+        misc.imsave(outdir+''+str(ittrNumb)+suf+"_comp.jpg",compositeImage)
 
 
 def revx(inArr,ittrNumb,fileNo,suf,outdir):
-	arr = np.copy(inArr)
-	arr += middleNorm
-	arr *= divBy
-	newImage = np.reshape(arr, (33,132,3))
-	misc.imsave(outdir+''+str(ittrNumb)+suf+".jpg",newImage)
+    arr = np.copy(inArr)
+    arr += middleNorm
+    arr *= divBy
+    newImage = np.reshape(arr, (33,132,3))
+    misc.imsave(outdir+''+str(ittrNumb)+suf+".jpg",newImage)
 
